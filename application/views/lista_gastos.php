e401386f353a0a256316d64656aa9ed19b09deab
Arthur Queiroz <tuckqueiroz82@gmail.com>
2021-02-11 11:58:38 -0300

    Aplicando conteúdos da aula de views e controllers

---
+++ application/views/lista_gastos.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Lista de gastos de fevereiro</h1>
+    <table>
+        <tr>
+            <td>Data</td>
+            <td>Descrição</td>
+            <td>Valor</td>
+        </tr>
+        <tr>
+            <td>01/02/2021</td>
+            <td>Taxi</td>
+            <td>4,00</td>
+        </tr>
+    </table>
+</body>
+</html>
